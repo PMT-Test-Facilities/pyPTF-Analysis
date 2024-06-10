939327ab8afb00fe9d85c748c71fff9b2ec790a5
Ben Smithers <b.smithers.actual@gmail.com>
2024-06-10 20:21:42 +0900

    updated readme

--- README.md
+++ README.md
@@ -1,3 +1,45 @@
+# Setting the environment
+
+First, you'll need to append the folder containing this one to your python path. 
+You can run this 
+```
+    export PYTHONPATH=$PYTHONPATH:$PWD
+```
+from this folder, or add a line like 
+```
+    export PYTHONPATH=$PYTHONPATH:/path/to/here
+```
+to your `.bash_profile` or `.bashrc` file.
+
+# Running the code 
+
+This assumes you've already run the `midas2root_ptf` code and processed the midas files. 
+
+You can probably run this on the ptf computer, but you can alternatively run it locally. 
+
+For the case of the latter, download one of the root files.  
+Make a `data` folder next to  the `pyPTF` folder, `cd` into it, and download the file you want to run the analysis on.
+Just run 
+```
+scp user@host.triumf.ca:~online/data/out_run05653.root .
+```
+after changing the `user` and `host` for the PTF computer. 
 
 
-Use `extract_pulses.py` to extract the root files and calculate the pulse amplitudes, means, etc. 
+Then you can use `extract_pulses.py` in `pyPTF` on one of the run files to process them into hdf5 files.
+
+For run 5653, you'd run 
+```
+    python extract_pulses.py ../data/out_run05653.root 5653
+```
+
+That makes a `pulse_series_fit` file with the information for pulses found in each of the waveforms. 
+You can run a charge or timing analysis using the files in the `analysis` folder. 
+
+```
+python charge.py ../../data/pulse_series_fit_run5653.hdf5
+python timing.py ../../data/pulse_series_fit_run5653.hdf5
+``` 
+
+
+
--- pyPTF/analyses/plot_waveforms.py
+++ pyPTF/analyses/plot_waveforms.py
@@ -9,41 +9,42 @@ PTF_TS = np.linspace(0, float(SAMPLESIZE)*1000/PTF_CAEN_V1730_SAMPLE_RATE, SAMPL
 
 
 data = h5.File(os.path.join(
-    os.path.dirname(__file__), "..","..","data","convert_V1730_wave2.h5"
+    os.path.dirname(__file__), "..","..","data","convert_V1730_wave1.h5"
 ), 'r')
 
 meta = h5.File(os.path.join(
-    os.path.dirname(__file__), "..","..","data","out_run05633.hdf5"
+    os.path.dirname(__file__), "..","..","data","out_run05653.hdf5"
 ), 'r')
 
 xcord = meta["gantry0_x"][:]
 ycord = meta["gantry0_y"][:]
 
 for j, scankey in enumerate(data.keys()):
-    if j%500!=0: # plot every hundredth
-        continue
+
 
     i = int(scankey.split("_")[1])
+    if i%50!=0:
+        continue
     these_waveforms = np.array(data[scankey], dtype=float)
 
     cut = np.array(range(len(these_waveforms)))%10 != 0
     
     these_waveforms = these_waveforms[cut]
+    print(len(these_waveforms))
+    for iw, wave in enumerate(these_waveforms):
+        plt.clf()
+        alpha = 0.1*abs(8140 - np.min(wave))/240
+        alpha = max([min([0, alpha]),0.5])
 
-    for wave in these_waveforms:
-        alpha = (8140 - np.min(wave))/60
-        alpha = max([min([0, alpha]),0.1])
-
-
-        plt.plot(PTF_TS, wave, alpha=alpha, color='k')
+        plt.plot(range(140), wave, alpha=alpha, color='k')
         #plt.plot(49 + np.array(range(len(wave))), wave, alpha=alpha, color='k')
+        #plt.ylim([-5, 150])
         plt.xlabel("Time [ns]", size=14)
         plt.ylabel("ADC Counts" ,size=14)
         plt.title("{:.3f}-{:.3f}".format(xcord[i], ycord[i]))
-    plt.show()
-   
-    plt.savefig("./waveforms/wave_{}.png".format(i))
-    if i==109:
-        plt.show()
-    plt.clf()
+        plt.show(block=False)   
+        plt.pause(.005)
+#plt.savefig("./waveforms/wave_{}.png".format(i))
+
+    
     print("./waveforms/wave_{}.png".format(i))
