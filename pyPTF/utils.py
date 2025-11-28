49a7d07bc86403d419b3cea1465cd035ec3beb91
Ben Smithers <b.smithers.actual@gmail.com>
2025-11-27 16:55:34 -0800

    utility for 3D plotting

--- pyPTF/utils.py
+++ pyPTF/utils.py
@@ -15,7 +15,7 @@ from scipy.interpolate import griddata, RectBivariateSpline
 import numpy as np 
 from math import log10,sqrt 
 
-DEBUG  = True
+DEBUG  = False
 
 pmt_x = 0.417
 pmt_y = 0.297 
@@ -512,7 +512,7 @@ class Irregular2DInterpolator:
             values=self._values, 
             xi=(mesh_x, mesh_y),
             method=interpolation,
-            fill_value=1.0
+            fill_value=0.0
         )
         
         # if there are any nans, scipy 
@@ -532,3 +532,35 @@ class Irregular2DInterpolator:
             return self._data_int( xs, ys ,grid=grid)
         else:
             return 10**self._data_int( xs, ys ,grid=grid)
+        
+
+
+def set_axes_equal(ax):
+    '''Make axes of 3D plot have equal scale so that spheres appear as spheres,
+    cubes as cubes, etc..  This is one possible solution to Matplotlib's
+    ax.set_aspect('equal') and ax.axis('equal') not working for 3D.
+
+    Input
+    ax: a matplotlib axis, e.g., as output from plt.gca().
+    
+    FROM: https://stackoverflow.com/questions/13685386/matplotlib-equal-unit-length-with-equal-aspect-ratio-z-axis-is-not-equal-to
+    '''
+
+    x_limits = ax.get_xlim3d()
+    y_limits = ax.get_ylim3d()
+    z_limits = ax.get_zlim3d()
+
+    x_range = abs(x_limits[1] - x_limits[0])
+    x_middle = np.mean(x_limits)
+    y_range = abs(y_limits[1] - y_limits[0])
+    y_middle = np.mean(y_limits)
+    z_range = abs(z_limits[1] - z_limits[0])
+    z_middle = np.mean(z_limits)
+
+    # The plot bounding box is a sphere in the sense of the infinity
+    # norm, hence I call half the max range the plot radius.
+    plot_radius = 0.4*max([x_range, y_range, z_range])
+
+    ax.set_xlim3d([x_middle - plot_radius, x_middle + plot_radius])
+    ax.set_ylim3d([y_middle - plot_radius, y_middle + plot_radius])
+    ax.set_zlim3d([z_middle - plot_radius, z_middle + plot_radius])
